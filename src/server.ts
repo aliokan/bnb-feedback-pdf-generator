3ed62a862149ffddb4480962d21fc63928cb2162
Laurent Deketelaere <laurent.deketelaere@redpencil.io>
2023-08-29 10:12:43 +0200

    feat: Add partial municipality rendering

--- src/server.ts
+++ src/server.ts
@@ -14,13 +14,31 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static("public"));
 // fonts and icons from ember-appuniversum
-app.use("/fonts", express.static(path.join(__dirname, '..', 'node_modules/@appuniversum/ember-appuniversum/public/fonts/')));
-app.use("/icons", express.static(path.join(__dirname, '..', 'node_modules/@appuniversum/ember-appuniversum/public/icons/')));
+app.use(
+  "/fonts",
+  express.static(
+    path.join(
+      __dirname,
+      "..",
+      "node_modules/@appuniversum/ember-appuniversum/public/fonts/"
+    )
+  )
+);
+app.use(
+  "/icons",
+  express.static(
+    path.join(
+      __dirname,
+      "..",
+      "node_modules/@appuniversum/ember-appuniversum/public/icons/"
+    )
+  )
+);
 
 app.get("/", (req, res) => {
-  const municipalities = [... new Set(data.map(
-    ({ municipality }) => municipality
-  ))].sort();
+  const municipalities = [
+    ...new Set(data.map(({ municipality }) => municipality)),
+  ].sort();
 
   res.render("index", {
     title: "FEEDBACK GELINKT PUBLICEREN EN MELDEN",
@@ -28,21 +46,37 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/:municipality", (req, res) => {
+const renderMunicipality = (
+  res: express.Response,
+  view: string,
+  municipality: string
+) => {
   const municipalityDatas = data.filter(
-    ({ municipality }) => municipality === req.params.municipality
+    ({ municipality: municipalityName }) => municipalityName === municipality
   );
 
   if (municipalityDatas.length === 0) {
     res.status(404).send("Sorry cant find that!");
   } else {
-    res.render("municipality", {
+    res.render(view, {
       title: "FEEDBACK GELINKT PUBLICEREN EN MELDEN",
-      municipality: req.params.municipality,
+      municipality: municipality,
       attributeDescriptions,
       data: municipalityDatas,
     });
   }
+};
+
+app.get("/:municipality", (req, res) => {
+  renderMunicipality(res, "municipality-all", req.params.municipality);
+});
+
+app.get("/:municipality/introduction", (req, res) => {
+  renderMunicipality(res, "municipality-introduction", req.params.municipality);
+});
+
+app.get("/:municipality/tables", (req, res) => {
+  renderMunicipality(res, "municipality-tables", req.params.municipality);
 });
 
 app.listen(3000, () => {
