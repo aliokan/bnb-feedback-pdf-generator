e580b61b7d53b668db901948131983aebca1f38e
Laurent Deketelaere <laurent.deketelaere@redpencil.io>
2023-09-13 10:58:59 +0200

    feat(municipality): Update description attributes

--- src/data/attributeDescription.ts
+++ src/data/attributeDescription.ts
@@ -2,111 +2,111 @@ export const attributeDescriptions = [
   {
     name: "Agendapunt - beschrijving",
     description:
-      "Korte beschrijving van het agendapunt. \nHet resultaat geeft aan hoe vaak we een beschrijving vinden voor het aantal aanwezige agendapunten",
+      "Korte beschrijving van het agendapunt. \nHet resultaat geeft aan hoe vaak we een beschrijving vinden voor het aantal aanwezige agendapunten. ",
   },
   {
     name: "Agendapunt - titel",
     description:
-      "De naam van het agendapunt (opschrift). \nDit attribuut hoort bij elke zitting aanwezig te zijn. Het resultaat geeft aan hoe vaak we een titel vinden voor het aantal gevonden agendapunten",
+      "De naam van het agendapunt (opschrift). \nDit attribuut hoort bij elke zitting aanwezig te zijn. Het resultaat geeft aan hoe vaak we een titel vinden voor het aantal gevonden agendapunten. ",
   },
   {
     name: "Artikel - inhoud",
     description:
-      "De inhoud van het artikel. \nHet resultaat geeft weer hoe vaak we een inhoud vinden voor het aantal gevonden artikels",
+      "De inhoud van het artikel. \nHet resultaat geeft weer hoe vaak we een inhoud vinden voor het aantal gevonden artikels. ",
   },
   {
     name: "Artikel - nummer",
     description:
-      "De volgorde van het artikel. \nHet resultaat geeft aan hoe vaak we een nummer vinden voor het aantal gevonden artikels",
+      "De volgorde van het artikel. \nHet resultaat geeft aan hoe vaak we een nummer vinden voor het aantal gevonden artikels. ",
   },
   {
     name: "Besluit - beschrijving",
     description:
-      "Een beknopte beschrijving van het besluit. \nHet resultaat geeft aan hoe vaak we een beschrijving hebben gevonden voor het aantal gevonden besluiten",
+      "Een beknopte beschrijving van het besluit. \nHet resultaat geeft aan hoe vaak we een beschrijving hebben gevonden voor het aantal gevonden besluiten. ",
   },
   {
     name: "Besluit - inhoud",
     description:
-      "De inhoud van het besluit. \nDit attribuut hoort bij elke zitting aanwezig te zijn. Het resultaat geeft aan hoe vaak we een inhoud vinden voor het aantal gevonden besluiten",
+      "De inhoud van het besluit. \nDit attribuut hoort bij elke zitting aanwezig te zijn. Het resultaat geeft aan hoe vaak we een inhoud vinden voor het aantal gevonden besluiten. ",
   },
   {
     name: "Besluit - publicatiedatum",
     description:
-      "De officiële publicatiedatum van het besluit. \nHet resultaat geeft aan hoe vaak we een publicatiedatum terugvinden voor het aantal gevonden besluiten",
+      "De officiële publicatiedatum van het besluit. \nHet resultaat geeft aan hoe vaak we een publicatiedatum terugvinden voor het aantal gevonden besluiten. ",
   },
   {
     name: "Besluit - titel",
     description:
-      "De naam van het besluit (opschrift). \nDit attribuut hoort op elke zitting aanwezig te zijn. Het resultaat geeft aan hoe vaak we een titel vinden voor het aantal gevonden besluiten",
+      "De naam van het besluit (opschrift). \nDit attribuut hoort op elke zitting aanwezig te zijn. Het resultaat geeft aan hoe vaak we een titel vinden voor het aantal gevonden besluiten. ",
   },
   {
     name: "Stemming - aantalOnthouders",
     description:
-      "Een mandataris die als onthouder heeft gestemd op het onderwerp van de stemming. \nHet resultaat geeft aan hoe vaak we het aantal onthouders vinden voor het aantal gevonden stemmingen. We verwachten dat het resultaat 100% is",
+      "Een mandataris die als onthouder heeft gestemd op het onderwerp van de stemming. \nHet resultaat geeft aan hoe vaak we het aantal onthouders vinden voor het aantal gevonden stemmingen. We verwachten dat het resultaat 100% is. ",
   },
   {
     name: "Stemming - aantalTegenstanders",
     description:
-      "Een mandataris die als tegenstander heeft gestemd op het onderwerp van de stemming. \nHet resultaat geeft aan hoe vaak we het aantal tegenstanders vinden voor het aantal gevonden stemmingen. We verwachten dat het resultaat 100% is",
+      "Een mandataris die als tegenstander heeft gestemd op het onderwerp van de stemming. \nHet resultaat geeft aan hoe vaak we het aantal tegenstanders vinden voor het aantal gevonden stemmingen. We verwachten dat het resultaat 100% is. ",
   },
   {
     name: "Stemming - aantalVoorstanders",
     description:
-      "Een mandataris die als voorstander heeft gestemd op het onderwerp van de stemming.  \nHet resultaat geeft aan hoe vaak we het aantal voorstanders vinden voor het aantal gevonden stemmingen. We verwachten dat het resultaat 100% is",
+      "Een mandataris die als voorstander heeft gestemd op het onderwerp van de stemming.  \nHet resultaat geeft aan hoe vaak we het aantal voorstanders vinden voor het aantal gevonden stemmingen. We verwachten dat het resultaat 100% is. ",
   },
   {
     name: "Stemming - geheim",
     description:
-      "Geeft aan of de stemming geheim was. \nDit attribuut geeft aan hoeveel van de stemmingen als geheime stemming zijn aageduid, en zal dus doorgaans geen resultaat van 100% behalen",
+      "Geeft aan of de stemming geheim was. \nDit attribuut geeft aan hoeveel van de stemmingen als geheime stemming zijn aageduid, en zal dus doorgaans geen resultaat van 100% behalen. ",
   },
   {
     name: "Stemming - gevolg",
     description:
-      "Voor elke stemming hoort het gevolg te worden aangegeven. Het resultaat geeft weer hoe vaak een gevolg voor een stemming hebben gevonden",
+      "Voor elke stemming hoort het gevolg te worden aangegeven. Het resultaat geeft weer hoe vaak een gevolg voor een stemming hebben gevonden. ",
   },
   {
     name: "Stemming - heeftaanwezige",
     description:
-      "Een mandataris (van het orgaan dat de zitting houdt) die aanwezig was tijdens de stemming. \nHet resultaat geeft aan hoe vaak we een aanwezige terugvinden voor het aantal gevonden stemmingen. We verwachten dat dit resultaat altijd 100% is.",
+      "Een mandataris (van het orgaan dat de zitting houdt) die aanwezig was tijdens de stemming. \nHet resultaat geeft aan hoe vaak we een aanwezige terugvinden voor het aantal gevonden stemmingen. We verwachten dat dit resultaat altijd 100% is. ",
   },
   {
     name: "Stemming - heeftOnthouder",
     description:
-      "Een mandataris die als onthouder heeft gestemd op het onderwerp van de stemming.  \nHet resultaat geeft aan hoe vaak we een onthouder terugvinden voor het aantal gevonden stemmingen. Het resultaat zal doorgaans niet 100% zijn.",
+      "Een mandataris die als onthouder heeft gestemd op het onderwerp van de stemming.  \nHet resultaat geeft aan hoe vaak we een onthouder terugvinden voor het aantal gevonden stemmingen. Het resultaat zal doorgaans niet 100% zijn. ",
   },
   {
     name: "Stemming - heeftTegenstander",
     description:
-      "Een mandataris die als tegenstander heeft gestemd op het onderwerp van de stemming. \nHet resultaat geeft aan hoe vaak we een tegenstander terugvinden voor het aantal gevonden stemmingen. Het resultaat zal doorgaans niet 100% zijn.",
+      "Een mandataris die als tegenstander heeft gestemd op het onderwerp van de stemming. \nHet resultaat geeft aan hoe vaak we een tegenstander terugvinden voor het aantal gevonden stemmingen. Het resultaat zal doorgaans niet 100% zijn. ",
   },
   {
     name: "Stemming - heeftVoorstander",
     description:
-      "Een mandataris die als voorstander heeft gestemd op het onderwerp van de stemming. \nHet resultaat geeft aan hoe vaak we een voorstander terugvinden voor het aantal gevonden stemmingen. We verwachten dat het resultaat doorgaans 100% is ",
+      "Een mandataris die als voorstander heeft gestemd op het onderwerp van de stemming. \nHet resultaat geeft aan hoe vaak we een voorstander terugvinden voor het aantal gevonden stemmingen. We verwachten dat het resultaat doorgaans 100% is. ",
   },
   {
     name: "Stemming - onderwerp",
     description:
-      "De beschrijving van het onderwerp waarover de stemming gaat. \nHet resultaat geeft aan hoe vaak we een onderwerp terugvinden voor het aantal gevonden stemmingen",
+      "De beschrijving van het onderwerp waarover de stemming gaat. \nHet resultaat geeft aan hoe vaak we een onderwerp terugvinden voor het aantal gevonden stemmingen. ",
   },
   {
     name: "Zitting - eind",
     description:
-      "Tijdstip waarop de zitting eindigt. Dit attribuut hoort bij elke zitting aanwezig te zijn. Het resultaat geeft aan hoe vaak we een einduur vinden voor de gevonden zittingen",
+      "Tijdstip waarop de zitting eindigt. Dit attribuut hoort bij elke zitting aanwezig te zijn. Het resultaat geeft aan hoe vaak we een einduur vinden voor de gevonden zittingen. ",
   },
   {
     name: "Zitting - geplandeStart",
     description:
-      "Het tijdstip waarop de zitting gepland is om te beginnen. Dit attribuut hoort bij elke zitting aanwezig te zijn. Het resultaat geeft aan hoe vaak we een geplande start vinden voor de gevonden zittingen",
+      "Het tijdstip waarop de zitting gepland is om te beginnen. Dit attribuut hoort bij elke zitting aanwezig te zijn. Het resultaat geeft aan hoe vaak we een geplande start vinden voor de gevonden zittingen. ",
   },
   {
     name: "Zitting - heeftAanwezigeBijStart",
     description:
-      "Een mandataris (van het bestuursorgaan die de zitting hield) die aanwezig was bij de start van de zitting. \nDit attribuut hoort bij elke zitting aanwezig te zijn. Het resultaat varieert afhankelijk van de aan- / afwezigheden bij een zitting.",
+      "Een mandataris (van het bestuursorgaan die de zitting hield) die aanwezig was bij de start van de zitting. \nDit attribuut hoort bij elke zitting aanwezig te zijn. Het resultaat varieert afhankelijk van de aan- / afwezigheden bij een zitting. ",
   },
   {
     name: "Zitting - start",
     description:
-      "Tijdstip waarop de zitting begint. Dit attribuut hoort bij elke zitting aanwezig te zijn. Het resultaat geeft aan hoe vaak we een startmoment vinden voor de gevonden zittingen",
+      "Tijdstip waarop de zitting begint. Dit attribuut hoort bij elke zitting aanwezig te zijn. Het resultaat geeft aan hoe vaak we een startmoment vinden voor de gevonden zittingen. ",
   },
 ];
